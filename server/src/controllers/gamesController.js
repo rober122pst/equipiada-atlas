c8a63d484f047cf17e23a962dcf0187aede417cb
DneyDev <sidney.rgmp@gmail.com>
2025-09-15 23:46:48 -0300

    Atualizações 15/09

---
+++ server/src/controllers/gamesController.js
@@ -0,0 +1,17 @@
+import Game from "../models/Games";
+export const createGame = async (req, res) => {
+    try {
+        const game = new Game(req.body);
+        await game.save();
+        res.status(201).json(game);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }};
+export const getGame = async (req, res) => {
+    try {
+        const game = await Game.findById(req.params.id);
+        if (!game) return res.status(404).json({ message: "Game not found" });
+        res.json(game);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }}; 
