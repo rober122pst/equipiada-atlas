2e12860654913741b7d7fa8ba1e12bed13b323a7
rober122pst <jrobertinhor.9@gmail.com>
2025-09-21 23:05:13 -0300

    home page de teste

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { Router, Routes, Route, Link } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 function App() {
     return (
         <Routes>
+            <Route path="/" element={<HomePage />} />
             <Route path="/profile/:userId" element={<ProfilePage />} />
             <Route path="/auth/login" element={<LoginPage />} />
             <Route path="/auth/register" element={<RegisterPage />} />
