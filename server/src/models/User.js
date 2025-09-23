a792b6e65c5512b83ed4971668984dcc34f20d46
DneyDev <sidney.rgmp@gmail.com>
2025-09-22 22:45:51 -0300

    models -> userController 'steamId' updated

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
     {
-        steamId: { type : String, required: true, unique: true },
+        steamId: { type : String, required: true, unique: true, sparse: true },
         name: { type : String, required: true }, 
         email: { type : String, required: true, unique: true },
         passwordHash: { type : String, required: true },
