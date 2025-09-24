f800545fc473bb26b93a50859176d75a838c6ee4
Dandanp3 <daniel.pyoliverp@gmail.com>
2025-09-23 23:11:12 -0300

    Cadastro via steam

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use("/auth", authRoutes)
 app.use("/users", userRoutes)
 app.use("/games", gamesRoutes)
-app.use("/users", userGamesRoutes)
+app.use("/users-games", userGamesRoutes)
 
 dotenv.config();
 
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import { steamLogin, addEmailSteamUser } from "../controllers/authSteamController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser)
+
+router.post("/steam", steamLogin)
+router.post("/steam/email", addEmailSteamUser)
 
 export default router;
