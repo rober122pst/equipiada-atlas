e8a56dbcbc656cbaa46ef136f28e7f2eef88b943
rober122pst <jrobertinhor.9@gmail.com>
2025-10-22 15:27:46 -0300

    fix:cards-background

--- client/src/components/ContainerBox.jsx
+++ client/src/components/ContainerBox.jsx
@@ -1,6 +1,6 @@
 function ContainerBox(props) {
     return (
-        <div className="flex border-2 border-rich-900 p-4 rounded-3xl w-full min-h-[100px] flex-col">
+        <div className="flex border-2 bg-gradient-to-br from-rich-900/40 to-transparent border-rich-900 p-4 rounded-3xl w-full min-h-[100px] flex-col">
             <h6 className="font-semibold text-2xl text-gray">{props.title}</h6>
             {props.children}
         </div>
--- client/src/components/ProfileStatsBar.jsx
+++ client/src/components/ProfileStatsBar.jsx
@@ -2,7 +2,7 @@ import { LuClock, LuGamepad, LuFlag, LuMedal, LuTrophy } from "react-icons/lu";
 
 function ProfileStatsBar({ userGames }) {
     return (
-        <div className="flex justify-around items-center p-4 border-2 rounded-2xl border-rich-900 w-full h-[116px]">
+        <div className="flex bg-gradient-to-br from-rich-900/40 to-transparent justify-around items-center p-4 border-2 rounded-2xl border-rich-900 w-full h-[116px]">
             <a href="" className="flex gap-2 items-center">
                 <LuClock className="text-raspberry text-5xl" />
                 <div>
